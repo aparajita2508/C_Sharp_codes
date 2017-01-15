de6c8a409a7f4dd0e85611a51208df8846077252
aparajita2508 <aparajita2508@gmail.com>
2017-01-15 21:56:31 +0530

    Hello world! program

---
+++ classes/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Learning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, world!");
+            Console.WriteLine("Chandaramani Tiwary is a duffer:");
+            Console.ReadLine();
+        }
+    }
+}
